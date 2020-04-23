3055ed9136c7d9d6cc7d571c24a3962574137af8
官维 <guanwei@triumen.com>
2020-04-23 17:10:19 +0800

    防抖节流

--- test/test.js
+++ test/test.js
@@ -181,6 +181,7 @@
 // console.log(type.getAge());
 // console.log(type.getName());
 
+
 // ------------------------------------------ this 的指向
 // 在 ES5 中，其实 this 的指向，始终坚持一个原理：this 永远指向最后调用它的那个对象
 // var name = "windowsName";
@@ -195,8 +196,14 @@
 
 // fn()
 
+
 // ---------------------------------------- 阻止事件冒泡
 // 在W3c中，使用stopPropagation()方法；在IE下设置cancelBubble = true
+
+
+// ---------------------------------------- 事件的各个阶段
+// 1：捕获阶段 ---> 2：目标阶段 ---> 3：冒泡阶段
+
 
 // ---------------------------------------- new 做了什么
 // 1、创建一个对象
@@ -228,7 +235,7 @@ function bubbleSort(arr = [], flag = true) {
 }
 console.log(bubbleSort([1, 5, 4, 9, 2, 11, 8, 7], false));
 
-// 选择排序  找到数据结构中的最小值并将其放置在第一位,接着找到第二个最小值并将其放到第二位,依次类推.
+// 选择排序  找到数据结构中的最小值并将其放置在第一位, 接着找到第二个最小值并将其放到第二位, 依次类推.
 function selectSort(arr) {
     let len = arr.length
     let idx
@@ -272,3 +279,100 @@ function quickSort(arr) {
     return quickSort(left).concat([pivot], quickSort(right))
 }
 console.log(quickSort([1, 5, 4, 9, 2, 11, 8, 7]));
+
+// 斐波那契数列
+function fib(n) {
+    let obj = {}
+    if (n <= 0) return
+    if (n == 1 || n == 2) {
+        obj[n] = 1
+        return 1
+    }
+
+    if (obj[n]) {
+        return obj[n]
+    } else {
+        obj[n] = fib(n - 1) + fib(n - 2)
+        return obj[n]
+    }
+}
+console.log(fib(10));
+
+
+// ---------------------------------------- js 操作 DOM
+// var app = document.querySelector("#app")
+// var p = document.createElement("p")
+// p.innerHTML = "创建p"
+// app.appendChild(p)
+// var p1 = document.querySelector("#p1")
+// app.removeChild(p1)
+// var p2 = document.querySelector("#p2")
+// var p3 = document.createElement("p")
+// p3.innerHTML = "p3"
+// app.insertBefore(p3, p2)
+
+
+// ---------------------------------------- 数组去重
+// es6
+let arr = [1, 2, 3, 2, 3, 4, 5]
+let newArr = [...new Set(arr)]
+console.log(newArr);
+
+// es5
+function unique(arr) {
+    let newArr = []
+    for (let i = 0, len = arr.length; i < len; i++) {
+        if (newArr.indexOf(arr[i]) === -1) {
+            newArr.push(arr[i])
+        }
+    }
+    return newArr
+}
+console.log(unique(arr));
+
+
+// --------------------------------------- 防抖、节流
+// 防抖： 函数防抖（debounce）是指在一定时间内，在动作被连续频繁触发的情况下，动作只会被执行一次，也就是说当调用动作过n毫秒后，才会执行该动作，若在这n毫秒内又调用此动作则将重新计算执行时间，所以短时间内的连续动作永远只会触发一次。
+function debounce(fn, delay) {
+    let timeId = null
+
+    return function () {
+        if (timeout !== null) clearTimeout(timeId);
+
+        let _this = this
+        let args = arguments
+
+        timeId = setTimeout(function () {
+            fn.apply(_this, args)
+        }, delay)
+    }
+}
+
+let debounceButton = document.querySelector('.debounce')
+let clickFn = function () {
+    console.log("1111111");
+}
+debounceButton.addEventListener("click", debounce(clickFn, 1500))
+
+// 节流: 函数节流是指一定时间内执行的操作只执行一次，也就是说即预先设定一个执行周期，当调用动作的时刻大于等于执行周期则执行该动作，然后进入下一个新周期，一个比较形象的例子是如果将水龙头拧紧直到水是以水滴的形式流出，那你会发现每隔一段时间，就会有一滴水流出。
+function throttle(fn, time) {
+    let timeId = null
+
+    return function () {
+        let _this = this
+        let args = arguments
+
+        if (!timeId) {
+            timeId = setTimeout(function () {
+                fn.apply(_this, args)
+                timeId = null
+            }, time)
+        }
+    }
+}
+
+let throttleButton = document.querySelector('.throttle')
+let clickFn1 = function () {
+    console.log("2222222");
+}
+throttleButton.addEventListener('click', throttle(clickFn1, 1000))
