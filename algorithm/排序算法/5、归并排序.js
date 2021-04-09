91499e49644d4ef5733e722dccb02c1a7a5a9f9a
gweid <guanwei006@gmail.com>
2021-04-09 18:16:27 +0800

    修改

---
+++ algorithm/排序算法/5、归并排序.js
@@ -0,0 +1,5 @@
+function mergeSort(arr) {
+
+}
+
+console.log('归并排序', countSort([1, 8, 4, 9, 6, 7, 2]))
--- algorithm/排序算法/5、计数排序.js
+++
@@ -1,5 +0,0 @@
-function countSort(arr) {
-
-}
-
-console.log('计数排序', countSort([1, 8, 4, 9, 6, 7, 2]))
