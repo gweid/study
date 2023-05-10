51faebbcb9fa59711f99732215e34b437c8cec35
guanwei <guanw01@mingyuanyun.com>
2023-05-10 17:47:25 +0800

    优化ts类型编程代码

--- typescript/ts类型编程/基础认知/test.ts
+++ typescript/ts类型编程/基础认知/test.ts
@@ -8,7 +8,7 @@ type res = isTrue<1>
 // 类型推导：infer 提取元组类型的第一个元素
 type IFirst<Tuple extends unknown[]> = Tuple extends [infer T, ...infer R] ? T : never
 
-type res1 = IFirst<[1, 2]>
+type FirstRes = IFirst<[1, 2]>
 
 // 联合、交叉类型
 // 联合
@@ -23,15 +23,16 @@ type IObj1 = 'aaa' & 'bbb' // never
 type IMap<T> = {
   [key in keyof T]: T[key]
 }
+type MapRes = IMap<{name: 'jack', age: 20}>
 
 // 类型映射 改变值
 type IMapVal<T> = {
   [key in keyof T]: [T[key], T[key]]
 }
-type res2 = IMapVal<{name: 'jack', age: 20}>
+type MapValRes = IMapVal<{name: 'jack', age: 20}>
 
-// 类型映射 改变索引值key，用 as 运算符，叫做重映射
+// 类型映射 改变索引值key，用 as 运算符，叫做重映
 type IMapKey<T> = {
   [key in keyof T as `${key & string}${key & string}`]: T[key]
 }
-type res3 = IMapKey<{name: 'jack', age: 20}>
+type MapKeyRes = IMapKey<{name: 'jack', age: 20}>
