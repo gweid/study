3811e36b7747a4c4fee911f8cecbc4b21d2a97eb
guanwei <guanw01@mingyuanyun.com>
2025-03-19 15:39:32 +0800

    ts套路---联合类型补充

--- typescript/ts类型编程/Demo/06.联合分散可简化.ts
+++ typescript/ts类型编程/Demo/06.联合分散可简化.ts
@@ -6,8 +6,18 @@ type Union = 'a' | 'b' | 'c'
 type UppercaseUnion<Str extends string> = Str extends 'a' ? Uppercase<Str> : Str
 type UppercaseUnionRes = UppercaseUnion<Union>
 
+
+// ------------ 联合类型转下划线转驼峰
+type CamelcaseUnion<Item extends string>
+  = Item extends `${infer First}_${infer Right}${infer Rest}`
+    ? `${First}${Uppercase<Right>}${CamelcaseUnion<Rest>}`
+    : Item
+type CamelcaseUnionRes = CamelcaseUnion<'first_name_category' | 'last_name_category'>
+
+
 // ------------ BEM
-// type testUnion = ['name', 'age'][number]
+// 数组转联合类型
+type testUnion = ['name', 'age'][number]
 
 type BEN<
   Block extends string,
